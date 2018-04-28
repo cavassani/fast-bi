e96f894242f648cf16ccff75f0e678c9f47fce01
cavassani <marceloscavassani@gmail.com>
2018-04-28 00:56:28 -0300

    footer novo

--- index.php
+++ index.php
@@ -530,12 +530,28 @@
     </div> -->
 
   <!-- FOOTER  -->
-    <footer class="container py-1">
+    <footer class="footer-bi">
+    <div class="container py-2">
       <div class="row">
-        <div class="col-12 col-md d-block text-center">
+        <div class="col-3 col-md d-block pt-3 footer-p ">
           <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg> -->
-          <small class="d-block mb-3 text-muted"> Fast BI &copy; 2018</small>
+          <p class="d-block mb-3 text-muted"> Fast BI &copy; 2018</p>
         </div>
+        <div class="col-6 col-md d-block  pt-3 social-list">
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="https://www.facebook.com/fastbisolucoes/?ref=br_rs"><i class="fa fa-facebook "></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a href=""><i class="fa fa-instagram "></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a href=""><i class="fa fa-linkedin "></i></a>
+          </li>
+
+        </ul>
+        </div>
+      </div>
       </div>
     </footer>
 
